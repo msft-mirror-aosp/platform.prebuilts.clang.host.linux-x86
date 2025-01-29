98d3c628eaa366985d4212905bfdb35a47962e37
Pirama Arumuga Nainar <pirama@google.com>
2025-01-29 22:53:06 +0000

    Update prebuilt Clang to r547379 (20.0.0).
    clang 20.0.0 (based on r547379) from build 12976843.
    
    Bug: http://b/379133546
    Test: N/A
    Change-Id: Ia4354debdf930d6a86bc858fa1a4f01dd84529ba

--- clang-r547379/include/llvm/Support/VCSRevision.h
+++ clang-r547379/include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
 #define LLVM_REVISION "b718bcaf8c198c82f3021447d943401e3ab5bd54"
-#define LLVM_REPOSITORY "/mnt/disks/build-disk/src/android/llvm-toolchain/out/llvm-project/llvm"
+#define LLVM_REPOSITORY "/mnt/disks/build-disk/src/android/llvm-r547379-release/out/llvm-project/llvm"
