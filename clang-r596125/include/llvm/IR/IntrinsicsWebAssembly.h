9b144befdfd93b90e02c663504fb9f4b95f9faf8
Venkatesh Srinivasan <venk@google.com>
2026-03-23 23:11:52 +0000

    Update prebuilt Clang to r596125 (22.0.2).
    clang 22.0.2 (based on r596125) from build 15071444.
    Test: N/A
    
    Bug: 472333761
    Change-Id: Ifd582b994a2fc6aab29bc04274b973a3a0add668

---
+++ clang-r596125/include/llvm/IR/IntrinsicsWebAssembly.h
@@ -0,0 +1,91 @@
+/*===- TableGen'erated file -------------------------------------*- C++ -*-===*\
+|*                                                                            *|
+|* Intrinsic Function Source Fragment                                         *|
+|*                                                                            *|
+|* Automatically generated file, do not edit!                                 *|
+|*                                                                            *|
+\*===----------------------------------------------------------------------===*/
+
+#ifndef LLVM_IR_INTRINSIC_WASM_ENUMS_H
+#define LLVM_IR_INTRINSIC_WASM_ENUMS_H
+namespace llvm::Intrinsic {
+enum WASMIntrinsics : unsigned {
+// Enum values for intrinsics.
+    wasm_alltrue = 14002,                              // llvm.wasm.alltrue (IntrinsicsWebAssembly.td:221)
+    wasm_anytrue,                              // llvm.wasm.anytrue (IntrinsicsWebAssembly.td:217)
+    wasm_avgr_unsigned,                        // llvm.wasm.avgr.unsigned (IntrinsicsWebAssembly.td:209)
+    wasm_bitmask,                              // llvm.wasm.bitmask (IntrinsicsWebAssembly.td:225)
+    wasm_bitselect,                            // llvm.wasm.bitselect (IntrinsicsWebAssembly.td:213)
+    wasm_catch,                                // llvm.wasm.catch (IntrinsicsWebAssembly.td:147)
+    wasm_dot,                                  // llvm.wasm.dot (IntrinsicsWebAssembly.td:229)
+    wasm_extadd_pairwise_signed,               // llvm.wasm.extadd.pairwise.signed (IntrinsicsWebAssembly.td:257)
+    wasm_extadd_pairwise_unsigned,             // llvm.wasm.extadd.pairwise.unsigned (IntrinsicsWebAssembly.td:261)
+    wasm_extract_lane_f16x8,                   // llvm.wasm.extract.lane.f16x8 (IntrinsicsWebAssembly.td:358)
+    wasm_get_ehselector,                       // llvm.wasm.get.ehselector (IntrinsicsWebAssembly.td:140)
+    wasm_get_exception,                        // llvm.wasm.get.exception (IntrinsicsWebAssembly.td:138)
+    wasm_landingpad_index,                     // llvm.wasm.landingpad.index (IntrinsicsWebAssembly.td:154)
+    wasm_loadf16_f32,                          // llvm.wasm.loadf16.f32 (IntrinsicsWebAssembly.td:344)
+    wasm_lsda,                                 // llvm.wasm.lsda (IntrinsicsWebAssembly.td:159)
+    wasm_memory_atomic_notify,                 // llvm.wasm.memory.atomic.notify (IntrinsicsWebAssembly.td:179)
+    wasm_memory_atomic_wait32,                 // llvm.wasm.memory.atomic.wait32 (IntrinsicsWebAssembly.td:167)
+    wasm_memory_atomic_wait64,                 // llvm.wasm.memory.atomic.wait64 (IntrinsicsWebAssembly.td:173)
+    wasm_memory_grow,                          // llvm.wasm.memory.grow (IntrinsicsWebAssembly.td:24)
+    wasm_memory_size,                          // llvm.wasm.memory.size (IntrinsicsWebAssembly.td:22)
+    wasm_narrow_signed,                        // llvm.wasm.narrow.signed (IntrinsicsWebAssembly.td:234)
+    wasm_narrow_unsigned,                      // llvm.wasm.narrow.unsigned (IntrinsicsWebAssembly.td:238)
+    wasm_pmax,                                 // llvm.wasm.pmax (IntrinsicsWebAssembly.td:252)
+    wasm_pmin,                                 // llvm.wasm.pmin (IntrinsicsWebAssembly.td:248)
+    wasm_q15mulr_sat_signed,                   // llvm.wasm.q15mulr.sat.signed (IntrinsicsWebAssembly.td:243)
+    wasm_ref_is_null_exn,                      // llvm.wasm.ref.is_null.exn (IntrinsicsWebAssembly.td:42)
+    wasm_ref_is_null_extern,                   // llvm.wasm.ref.is_null.extern (IntrinsicsWebAssembly.td:36)
+    wasm_ref_is_null_func,                     // llvm.wasm.ref.is_null.func (IntrinsicsWebAssembly.td:39)
+    wasm_ref_null_exn,                         // llvm.wasm.ref.null.exn (IntrinsicsWebAssembly.td:34)
+    wasm_ref_null_extern,                      // llvm.wasm.ref.null.extern (IntrinsicsWebAssembly.td:30)
+    wasm_ref_null_func,                        // llvm.wasm.ref.null.func (IntrinsicsWebAssembly.td:32)
+    wasm_ref_test_func,                        // llvm.wasm.ref.test.func (IntrinsicsWebAssembly.td:46)
+    wasm_relaxed_dot_bf16x8_add_f32,           // llvm.wasm.relaxed.dot.bf16x8.add.f32 (IntrinsicsWebAssembly.td:333)
+    wasm_relaxed_dot_i8x16_i7x16_add_signed,   // llvm.wasm.relaxed.dot.i8x16.i7x16.add.signed (IntrinsicsWebAssembly.td:328)
+    wasm_relaxed_dot_i8x16_i7x16_signed,       // llvm.wasm.relaxed.dot.i8x16.i7x16.signed (IntrinsicsWebAssembly.td:323)
+    wasm_relaxed_laneselect,                   // llvm.wasm.relaxed.laneselect (IntrinsicsWebAssembly.td:279)
+    wasm_relaxed_madd,                         // llvm.wasm.relaxed.madd (IntrinsicsWebAssembly.td:270)
+    wasm_relaxed_max,                          // llvm.wasm.relaxed.max (IntrinsicsWebAssembly.td:293)
+    wasm_relaxed_min,                          // llvm.wasm.relaxed.min (IntrinsicsWebAssembly.td:289)
+    wasm_relaxed_nmadd,                        // llvm.wasm.relaxed.nmadd (IntrinsicsWebAssembly.td:274)
+    wasm_relaxed_q15mulr_signed,               // llvm.wasm.relaxed.q15mulr.signed (IntrinsicsWebAssembly.td:318)
+    wasm_relaxed_swizzle,                      // llvm.wasm.relaxed.swizzle (IntrinsicsWebAssembly.td:284)
+    wasm_relaxed_trunc_signed,                 // llvm.wasm.relaxed.trunc.signed (IntrinsicsWebAssembly.td:298)
+    wasm_relaxed_trunc_signed_zero,            // llvm.wasm.relaxed.trunc.signed.zero (IntrinsicsWebAssembly.td:308)
+    wasm_relaxed_trunc_unsigned,               // llvm.wasm.relaxed.trunc.unsigned (IntrinsicsWebAssembly.td:303)
+    wasm_relaxed_trunc_unsigned_zero,          // llvm.wasm.relaxed.trunc.unsigned.zero (IntrinsicsWebAssembly.td:313)
+    wasm_replace_lane_f16x8,                   // llvm.wasm.replace.lane.f16x8 (IntrinsicsWebAssembly.td:362)
+    wasm_rethrow,                              // llvm.wasm.rethrow (IntrinsicsWebAssembly.td:134)
+    wasm_shuffle,                              // llvm.wasm.shuffle (IntrinsicsWebAssembly.td:193)
+    wasm_splat_f16x8,                          // llvm.wasm.splat.f16x8 (IntrinsicsWebAssembly.td:354)
+    wasm_storef16_f32,                         // llvm.wasm.storef16.f32 (IntrinsicsWebAssembly.td:349)
+    wasm_swizzle,                              // llvm.wasm.swizzle (IntrinsicsWebAssembly.td:189)
+    wasm_table_copy,                           // llvm.wasm.table.copy (IntrinsicsWebAssembly.td:76)
+    wasm_table_fill_exnref,                    // llvm.wasm.table.fill.exnref (IntrinsicsWebAssembly.td:97)
+    wasm_table_fill_externref,                 // llvm.wasm.table.fill.externref (IntrinsicsWebAssembly.td:89)
+    wasm_table_fill_funcref,                   // llvm.wasm.table.fill.funcref (IntrinsicsWebAssembly.td:93)
+    wasm_table_get_exnref,                     // llvm.wasm.table.get.exnref (IntrinsicsWebAssembly.td:69)
+    wasm_table_get_externref,                  // llvm.wasm.table.get.externref (IntrinsicsWebAssembly.td:63)
+    wasm_table_get_funcref,                    // llvm.wasm.table.get.funcref (IntrinsicsWebAssembly.td:66)
+    wasm_table_grow_exnref,                    // llvm.wasm.table.grow.exnref (IntrinsicsWebAssembly.td:86)
+    wasm_table_grow_externref,                 // llvm.wasm.table.grow.externref (IntrinsicsWebAssembly.td:80)
+    wasm_table_grow_funcref,                   // llvm.wasm.table.grow.funcref (IntrinsicsWebAssembly.td:83)
+    wasm_table_set_exnref,                     // llvm.wasm.table.set.exnref (IntrinsicsWebAssembly.td:59)
+    wasm_table_set_externref,                  // llvm.wasm.table.set.externref (IntrinsicsWebAssembly.td:53)
+    wasm_table_set_funcref,                    // llvm.wasm.table.set.funcref (IntrinsicsWebAssembly.td:56)
+    wasm_table_size,                           // llvm.wasm.table.size (IntrinsicsWebAssembly.td:74)
+    wasm_throw,                                // llvm.wasm.throw (IntrinsicsWebAssembly.td:132)
+    wasm_tls_align,                            // llvm.wasm.tls.align (IntrinsicsWebAssembly.td:377)
+    wasm_tls_base,                             // llvm.wasm.tls.base (IntrinsicsWebAssembly.td:382)
+    wasm_tls_size,                             // llvm.wasm.tls.size (IntrinsicsWebAssembly.td:372)
+    wasm_trunc_saturate_signed,                // llvm.wasm.trunc.saturate.signed (IntrinsicsWebAssembly.td:118)
+    wasm_trunc_saturate_unsigned,              // llvm.wasm.trunc.saturate.unsigned (IntrinsicsWebAssembly.td:121)
+    wasm_trunc_signed,                         // llvm.wasm.trunc.signed (IntrinsicsWebAssembly.td:107)
+    wasm_trunc_unsigned,                       // llvm.wasm.trunc.unsigned (IntrinsicsWebAssembly.td:110)
+}; // enum
+} // namespace llvm::Intrinsic
+#endif
+
---
+++ clang-r596125/include/llvm/IR/IntrinsicsXCore.h
@@ -0,0 +1,70 @@
+/*===- TableGen'erated file -------------------------------------*- C++ -*-===*\
+|*                                                                            *|
+|* Intrinsic Function Source Fragment                                         *|
+|*                                                                            *|
+|* Automatically generated file, do not edit!                                 *|
+|*                                                                            *|
+\*===----------------------------------------------------------------------===*/
+
+#ifndef LLVM_IR_INTRINSIC_XCORE_ENUMS_H
+#define LLVM_IR_INTRINSIC_XCORE_ENUMS_H
+namespace llvm::Intrinsic {
+enum XCOREIntrinsics : unsigned {
+// Enum values for intrinsics.
+    xcore_bitrev = 15765,                              // llvm.xcore.bitrev (IntrinsicsXCore.td:15)
+    xcore_checkevent,                          // llvm.xcore.checkevent (IntrinsicsXCore.td:100)
+    xcore_chkct,                               // llvm.xcore.chkct (IntrinsicsXCore.td:53)
+    xcore_clre,                                // llvm.xcore.clre (IntrinsicsXCore.td:102)
+    xcore_clrpt,                               // llvm.xcore.clrpt (IntrinsicsXCore.td:69)
+    xcore_clrsr,                               // llvm.xcore.clrsr (IntrinsicsXCore.td:36)
+    xcore_crc32,                               // llvm.xcore.crc32 (IntrinsicsXCore.td:20)
+    xcore_crc8,                                // llvm.xcore.crc8 (IntrinsicsXCore.td:17)
+    xcore_edu,                                 // llvm.xcore.edu (IntrinsicsXCore.td:82)
+    xcore_eeu,                                 // llvm.xcore.eeu (IntrinsicsXCore.td:81)
+    xcore_endin,                               // llvm.xcore.endin (IntrinsicsXCore.td:91)
+    xcore_freer,                               // llvm.xcore.freer (IntrinsicsXCore.td:40)
+    xcore_geted,                               // llvm.xcore.geted (IntrinsicsXCore.td:33)
+    xcore_getet,                               // llvm.xcore.getet (IntrinsicsXCore.td:34)
+    xcore_getid,                               // llvm.xcore.getid (IntrinsicsXCore.td:27)
+    xcore_getps,                               // llvm.xcore.getps (IntrinsicsXCore.td:29)
+    xcore_getr,                                // llvm.xcore.getr (IntrinsicsXCore.td:39)
+    xcore_getst,                               // llvm.xcore.getst (IntrinsicsXCore.td:105)
+    xcore_getts,                               // llvm.xcore.getts (IntrinsicsXCore.td:71)
+    xcore_in,                                  // llvm.xcore.in (IntrinsicsXCore.td:42)
+    xcore_inct,                                // llvm.xcore.inct (IntrinsicsXCore.td:45)
+    xcore_initcp,                              // llvm.xcore.initcp (IntrinsicsXCore.td:116)
+    xcore_initdp,                              // llvm.xcore.initdp (IntrinsicsXCore.td:118)
+    xcore_initlr,                              // llvm.xcore.initlr (IntrinsicsXCore.td:114)
+    xcore_initpc,                              // llvm.xcore.initpc (IntrinsicsXCore.td:112)
+    xcore_initsp,                              // llvm.xcore.initsp (IntrinsicsXCore.td:110)
+    xcore_inshr,                               // llvm.xcore.inshr (IntrinsicsXCore.td:63)
+    xcore_int,                                 // llvm.xcore.int (IntrinsicsXCore.td:43)
+    xcore_mjoin,                               // llvm.xcore.mjoin (IntrinsicsXCore.td:109)
+    xcore_msync,                               // llvm.xcore.msync (IntrinsicsXCore.td:107)
+    xcore_out,                                 // llvm.xcore.out (IntrinsicsXCore.td:47)
+    xcore_outct,                               // llvm.xcore.outct (IntrinsicsXCore.td:51)
+    xcore_outshr,                              // llvm.xcore.outshr (IntrinsicsXCore.td:65)
+    xcore_outt,                                // llvm.xcore.outt (IntrinsicsXCore.td:49)
+    xcore_peek,                                // llvm.xcore.peek (IntrinsicsXCore.td:89)
+    xcore_setc,                                // llvm.xcore.setc (IntrinsicsXCore.td:61)
+    xcore_setclk,                              // llvm.xcore.setclk (IntrinsicsXCore.td:83)
+    xcore_setd,                                // llvm.xcore.setd (IntrinsicsXCore.td:59)
+    xcore_setev,                               // llvm.xcore.setev (IntrinsicsXCore.td:79)
+    xcore_setps,                               // llvm.xcore.setps (IntrinsicsXCore.td:31)
+    xcore_setpsc,                              // llvm.xcore.setpsc (IntrinsicsXCore.td:87)
+    xcore_setpt,                               // llvm.xcore.setpt (IntrinsicsXCore.td:67)
+    xcore_setrdy,                              // llvm.xcore.setrdy (IntrinsicsXCore.td:85)
+    xcore_setsr,                               // llvm.xcore.setsr (IntrinsicsXCore.td:35)
+    xcore_settw,                               // llvm.xcore.settw (IntrinsicsXCore.td:75)
+    xcore_setv,                                // llvm.xcore.setv (IntrinsicsXCore.td:77)
+    xcore_sext,                                // llvm.xcore.sext (IntrinsicsXCore.td:23)
+    xcore_ssync,                               // llvm.xcore.ssync (IntrinsicsXCore.td:108)
+    xcore_syncr,                               // llvm.xcore.syncr (IntrinsicsXCore.td:73)
+    xcore_testct,                              // llvm.xcore.testct (IntrinsicsXCore.td:55)
+    xcore_testwct,                             // llvm.xcore.testwct (IntrinsicsXCore.td:57)
+    xcore_waitevent,                           // llvm.xcore.waitevent (IntrinsicsXCore.td:95)
+    xcore_zext,                                // llvm.xcore.zext (IntrinsicsXCore.td:25)
+}; // enum
+} // namespace llvm::Intrinsic
+#endif
+
